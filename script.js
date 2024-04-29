37b2f539a21147f910b0524858da62381e94b63e
tytoane <163216837+tytoane@users.noreply.github.com>
2024-04-29 16:17:43 +0200

    Header+ intro
    J'ai fait le header+hero+intro

---
+++ script.js
@@ -0,0 +1,18 @@
+// Envoie une requête GET à l'URL de l'api pour récupérer des données
+fetch("gameLab.json")
+  // Une fois que la réponse est reçue
+  .then(rep => {
+    // transforme la reponse obtenu en JSON 
+    return rep.json();
+  })
+  // Une fois que les données JSON sont extraites avec succès, jeles transmet à une fonction pour l'utiliser plus tard
+  .then(data => {
+    // Appelle la fonction addProducts() avec les données JSON en entrée
+    addProducts(data);
+  });
+
+// Cette fonction prend les données JSON en entrée et effectue une action sur celles-ci
+function addProducts(data) {
+  // Affiche les données dans la console du navigateur
+  console.log(data);
+}
